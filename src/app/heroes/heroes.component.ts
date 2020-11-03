10a7c01b13b1df4b401ccd19b0c9700f7d84fdfb
Stanislav Babachanov <statu7@gmx.de>
2020-11-03 19:51:51 +0100

    add heroes async service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import {HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,19 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void{
       this.selectedHero = hero;
+  }
+
+  
+
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
   hero: Hero =  {
@@ -21,9 +30,12 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  constructor() { }
+  
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService) { }
+
+
+  ngOnInit() {
+    this.getHeroes();
   }
-
 }
